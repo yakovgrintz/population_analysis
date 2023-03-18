77e0ef419a7ae76bb9696f23b3cd02874f9a6a9b
yakov grintz <yakovgrintz@gmail.com>
2023-03-18 20:19:20 +0200

    try add app to github pages

--- docs/app.py
+++ docs/app.py
@@ -1,9 +1,9 @@
-from dash import Dash, html, dcc, Input, Output, dash_table, State
+import os
+
+from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 
-from App.helpers_to_app.dashboards import create_dashboards
-from helpers_to_app.create_table_and_graphs import Table_func_create
-from helpers_to_app.create_table_and_graphs import Graphs
+from docs.helpers_to_app.dashboards import create_dashboards
 from helpers_to_app.db_functions import func_to_db
 
 # connect to db
@@ -14,7 +14,7 @@ cursor = mydb.cursor()
 cursor.execute(year_query)
 options = [{'label': row[0], 'value': row[0]} for row in cursor.fetchall()]
 mydb.close()
-app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR],assets_folder=os.path.join(os.getcwd(), 'docs'))
 app.layout = html.Div(children=[
     html.H1(children=' Israel Population Analysis Dashboard', style={'textAlign': 'center'}),
     html.P(["Welcome to my dashboard.", html.Br(),
